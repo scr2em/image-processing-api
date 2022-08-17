db6444f03a9a7342597ac43433d82ef21ac1116e
Mohamed M. Abdelgawad <mohammed.abdelgawad@leadliaison.com>
2022-08-17 20:21:19 +0200

    test: resize image

--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -1,5 +1,8 @@
 import request from "supertest";
+import path from "path";
+import fs from "fs";
 import app from "../server";
+import { isFileExist } from "../utils";
 
 it("gives status 404 for random routes", async () => {
   const response = await request(app).get("/qweq");
@@ -14,4 +17,16 @@ it("missing filename", async () => {
 it("serves image", async () => {
   const response = await request(app).get("/api/images?filename=1");
   expect(response.status).toEqual(200);
+});
+
+it("resize image ", async () => {
+  const filename = 1;
+  const width = 199;
+  const height = 199;
+  const expectedOutputDir = path.normalize(`${__dirname}/../thumbs/${filename}-${width}-${height}.jpg`);
+  if (await isFileExist(expectedOutputDir)) {
+    fs.unlinkSync(expectedOutputDir);
+  }
+  await request(app).get("/api/images?filename=1&width=199&height=199");
+  expect(await isFileExist(expectedOutputDir)).toEqual(true);
 });
